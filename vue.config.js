cc38c83d376b06997471d75084e6507fdfb9c58f
leeyunxiang <liyunxiang345@163.com>
2020-02-27 17:00:00 +0800

    项目配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,15 @@
 module.exports = {
   devServer: {
-    open: true
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        /* 目标代理服务器地址 */
+        target: 'http://cloudsor.site/',
+        /* 允许跨域 */
+        changeOrigin: true,
+      },
+    },
   },
   assetsDir: "static",
   filenameHashing: false, // 生成的静态资源在它们的文件名中包含了 hash 以便更好的控制缓存
