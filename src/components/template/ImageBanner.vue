3ce0488855ac203e0db0ab2759aea430d783b237
李云翔 <liyunxiang345@163.com>
2020-09-23 19:04:45 +0800

    css3 动画代替transition 组件

--- src/components/template/ImageBanner.vue
+++ src/components/template/ImageBanner.vue
@@ -1,10 +1,8 @@
 <template>
   <section class="image-banner">
-    <transition-group name="slide-fade">
-      <div class="img" v-for="(item,$index) in list" v-show="index === $index" :key="item.url" :style="{backgroundImage: `url(${item.url})`}">
-        <slot name="center"></slot>
-      </div>
-    </transition-group>
+    <div class="img" :class="index === $index ? 'img-transition' : ''" v-for="(item,$index) in list" v-show="index === $index" :key="item.url" :style="{backgroundImage: `url(${item.url})`}">
+      <slot name="center"></slot>
+    </div>
   </section>
 </template>
 <script>
@@ -28,8 +26,9 @@ export default {
       }else{
         this.index += 1;
       }
-      setTimeout(() => {
+      this.timer = setTimeout(() => {
         window.requestAnimationFrame(this.timeGo);
+        clearTimeout(this.timer);
       },12000)
     }
   },
@@ -46,6 +45,29 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+@keyframes fadeIn {
+	0% {
+	opacity: 0; /*初始状态 透明度为0*/
+  }
+  1% {
+    opacity: .1s;
+  }
+  2% {
+    opacity: .3s
+  }
+  5% {
+    opacity: .5s;
+  }
+  10% {
+    opacity: .9;
+  }
+  15% {
+    opacity: 1;
+  }
+	100% {
+		opacity: 1; /*结尾状态 透明度为1*/
+	}
+}
 .image-banner{
   width: 1440px;
   height: 1066px;
@@ -54,13 +76,12 @@ export default {
     height: 100%;
     background-size: cover;
     overflow: hidden;
+    opacity: 0;
   }
-}
-.slide-fade-enter-active {
-  transition: all 1.4s;
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active for below version 2.1.8 */ {
-  opacity: 0;
+  .img-transition{
+    animation-name: fadeIn;
+    animation-duration: 12s;
+    animation-iteration-count: 1;
+  }
 }
 </style>
